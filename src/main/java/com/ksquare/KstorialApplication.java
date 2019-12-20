e4c07b77ca93177e21c1313890dd97e7738467a7
Christian <blacklux_carch@hotmail.com>
2019-12-20 13:00:02 -0600

    First Commit
    First Commit

---
+++ src/main/java/com/ksquare/KstorialApplication.java
@@ -0,0 +1,13 @@
+package com.ksquare;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KstorialApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KstorialApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ksquare/KstorialApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ksquare;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KstorialApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
